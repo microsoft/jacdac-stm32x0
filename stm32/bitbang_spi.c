8e13db738bd9e0a4f54381e5f15a341df0b50c75
Michal Moskal <michal@moskal.me>
2020-04-10 07:35:20 -0700

    LOW_POWER mode

--- stm32/bitbang_spi.c
+++ stm32/bitbang_spi.c
@@ -3,54 +3,44 @@
 #define MASK_SET(p) (1 << ((p)&0xf))
 #define MASK_CLR(p) (1 << (((p)&0xf) + 16))
 
-// at 48MHz
-// 1 is around 1MHz - still works up to 1.25MHz or so
-// 5 is 660kHz
-// 10 is 430kHz
-#define NOP target_wait_cycles(5)
+#ifdef LOW_POWER
+#define NOP0 ((void)0)
+#define NOP1 asm volatile("nop \n nop \n nop")
+#else
+#define NOP0 target_wait_cycles(1)
+#define NOP1 target_wait_cycles(5)
+#endif
 
-#define SEND_BIT(n)                                                                                \
-    NOP;                                                                                           \
+#define SEND_BIT(n, wait0, wait1)                                                                  \
+    wait0;                                                                                         \
     ACC_PORT->BSRR = MASK_CLR(PIN_ACC_MOSI) | (((b >> n) & 1) << (PIN_ACC_MOSI & 0xf));            \
     ACC_PORT->BSRR = MASK_SET(PIN_ACC_SCK);                                                        \
-    NOP;                                                                                           \
+    wait1;                                                                                         \
     ACC_PORT->BSRR = MASK_CLR(PIN_ACC_SCK)
 
-#define RECV_BIT(n)                                                                                \
-    NOP;                                                                                           \
+#define RECV_BIT(n, wait0, wait1)                                                                  \
+    wait0;                                                                                         \
     ACC_PORT->BSRR = MASK_SET(PIN_ACC_SCK);                                                        \
-    NOP;                                                                                           \
+    wait1;                                                                                         \
     b |= ((ACC_PORT->IDR >> (PIN_ACC_MISO & 0xf)) & 1) << n;                                       \
     ACC_PORT->BSRR = MASK_CLR(PIN_ACC_SCK)
 
 void bspi_send(const void *src, uint32_t len) {
-    // target_wait_us(5);
     for (int i = 0; i < len; ++i) {
-        uint8_t b = ((uint8_t*)src)[i];
-        SEND_BIT(7);
-        SEND_BIT(6);
-        SEND_BIT(5);
-        SEND_BIT(4);
-        SEND_BIT(3);
-        SEND_BIT(2);
-        SEND_BIT(1);
-        SEND_BIT(0);
+        uint8_t b = ((uint8_t *)src)[i];
+        for (int j = 7; j >= 0; j--) {
+            SEND_BIT(j, NOP0, NOP1);
+        }
     }
     pin_set(PIN_ACC_MOSI, 0);
 }
 
 void bspi_recv(void *dst, uint32_t len) {
-    // target_wait_us(5);
     for (int i = 0; i < len; ++i) {
         uint8_t b = 0x00;
-        RECV_BIT(7);
-        RECV_BIT(6);
-        RECV_BIT(5);
-        RECV_BIT(4);
-        RECV_BIT(3);
-        RECV_BIT(2);
-        RECV_BIT(1);
-        RECV_BIT(0);
-        ((uint8_t*)dst)[i] = b;
+        for (int j = 7; j >= 0; j--) {
+            RECV_BIT(j, NOP0, NOP1);
+        }
+        ((uint8_t *)dst)[i] = b;
     }
 }
