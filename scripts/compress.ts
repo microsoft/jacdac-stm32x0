030079917e19f109e28f39016b76d80e0d6bd9da
Michal Moskal <michal@moskal.me>
2020-04-15 12:00:02 -0700

    Working compress (but bad)

--- scripts/compress.ts
+++ scripts/compress.ts
@@ -1,11 +1,14 @@
-const blockSize = 1024
+const blockSizeBits = 10
+const blockSize = 1 << blockSizeBits
 const blockSkip = 2
-const reflen = 2
+const refantigain = 2
 const bucketsBits = 12
+const maxsegsize = 64
 const minsegsize = 4
+let startgain = -4
 
 const segmentSizes: number[] = []
-for (let sz = blockSize; sz >= minsegsize; sz >>= 1)
+for (let sz = maxsegsize; sz >= minsegsize; sz >>= 1)
     segmentSizes.push(sz)
 const mingain = -10
 
@@ -16,15 +19,21 @@ class Segment {
     hash: number
     gain: number
     allstarts: number[]
+    idx = -1
 
     constructor(public start: number, public length: number) {
-        this.gain = -reflen
+        this.gain = startgain
         this.hash = 0
         this.allstarts = null
     }
 
     init() {
         this.hash = fnv1a(inputfile, this.start, this.length)
+    }
+
+    get score() {
+        if (this.gain < 0) return this.gain
+        return this.gain + (this.length << 20)
     }
 
     get bucketHash() {
@@ -63,13 +72,19 @@ class Segment {
     add(other: Segment) {
         if (other.start == this.start)
             throw "whoops"
-        this.gain += this.length - reflen
+        if (!other.equals(this))
+            throw "whoops"
+        this.gain += this.length - refantigain
         if (!this.allstarts) this.allstarts = [this.start]
         this.allstarts.push(other.start)
     }
 }
 
-function overlaps(s0: number, e0: number, s1: number, e1: number) {
+function overlaps(s0: number, l0: number, s1: number, l1: number) {
+    return overlapsSE(s0, s0 + l0, s1, s1 + l1)
+}
+
+function overlapsSE(s0: number, e0: number, s1: number, e1: number) {
     return (s0 <= s1 && s1 < e0) ||
         (s0 <= e1 && e1 < e0) ||
         (s1 <= s0 && s0 < e1) ||
@@ -77,17 +92,29 @@ function overlaps(s0: number, e0: number, s1: number, e1: number) {
 }
 
 class Bucket {
-    _maxgain: number = null
+    private _maxscore: Segment
     segments: Segment[] = []
 
-    get maxgain() {
-        if (this._maxgain == null) {
-            this._maxgain = mingain
+    clearCache() {
+        this._maxscore = undefined
+    }
+
+    get max() {
+        if (this._maxscore === undefined) {
+            this._maxscore = null
+            let numdrops = 0
             for (const s of this.segments) {
-                this._maxgain = Math.max(s.gain, this._maxgain)
+                if (s.gain > 0) {
+                    if (!this._maxscore || s.score > this._maxscore.score)
+                        this._maxscore = s
+                } else {
+                    numdrops++
+                }
             }
+            if (numdrops)
+                this.segments = this.segments.filter(s => s.gain > 0)
         }
-        return this._maxgain
+        return this._maxscore
     }
 }
 
@@ -122,33 +149,14 @@ function fnv1a(data: Uint8Array, start: number, len: number) {
 }
 
 
-function segmentsOverlapping(start: number, end: number) {
-    const fileOff = start & ~(blockSize - 1)
+function segmentsAt(fileOff: number) {
     const res: Segment[] = []
     for (let blockOff = fileOff; blockOff < fileOff + blockSize; blockOff += blockSkip) {
         for (const segSize of segmentSizes) {
-            if (blockOff + segSize <= fileOff + blockSize) {
-                const s = new Segment(blockOff, segSize)
-                if (s.isWithin(start, end) || s.contains(start, end)) {
-                    s.init()
-                    res.push(s)
-                }
-            }
-        }
-    }
-    return res
-}
-
-function compressBlock(fileOff: number) {
-    const res: Segment[] = []
-    for (let blockOff = fileOff; blockOff < fileOff + blockSize; blockOff += blockSkip) {
-        for (const segSize of segmentSizes) {
-            if (blockOff + segSize <= fileOff + blockSize) {
-                const s = new Segment(blockOff, segSize)
-                if (s.isWithin(start, end) || s.contains(start, end)) {
-                    s.init()
-                    res.push(s)
-                }
+            const s = new Segment(blockOff, segSize)
+            if (s.end <= fileOff + blockSize) {
+                s.init()
+                res.push(s)
             }
         }
     }
@@ -159,12 +167,15 @@ function compress(_input: Uint8Array) {
     const t0 = Date.now()
 
     if (_input.length & (blockSize - 1))
-        throw "oops"
+        throw "wrong input len"
     inputfile = _input
     buckets = new Array(1 << bucketsBits)
+    buckets.fill(null)
+
+    console.log(`input len: ${inputfile.length}`)
 
     for (let fileOff = 0; fileOff < inputfile.length; fileOff += blockSize) {
-        for (const s of segmentsOverlapping(fileOff, fileOff + blockSize)) {
+        for (const s of segmentsAt(fileOff)) {
             const s2 = findSegment(s)
             if (s2) {
                 s2.add(s)
@@ -175,61 +186,183 @@ function compress(_input: Uint8Array) {
         }
     }
 
+    const segsAt: Segment[][] = new Array(inputfile.length >> blockSizeBits)
+    for (let i = 0; i < segsAt.length; ++i)segsAt[i] = []
+
     let numsegs = 0
+    let numreps = 0
     let queue: Bucket[] = []
     for (const b of buckets) {
-        if (b && b.maxgain > 0) {
-            b.segments = b.segments.filter(s => s.gain > 0)
+        if (b && b.max && b.max.score > 0) {
+            b.segments = b.segments.filter(s => {
+                if (s.gain > 0) {
+                    if (!s.allstarts) s.allstarts = [s.start]
+                    for (const st of s.allstarts) {
+                        segsAt[st >> blockSizeBits].push(s)
+                        numreps++
+                    }
+                    return true
+                }
+                return false
+            })
             queue.push(b)
             numsegs += b.segments.length
         }
     }
 
     const t1 = Date.now()
-    console.log(`${numsegs} segments; ${queue.length} buckets; ${t1 - t0}ms`)
+    console.log(`${numsegs} segments (${numreps} reps); ${queue.length} buckets; ${t1 - t0}ms`)
+
+    const cover: Segment[] = new Array(inputfile.length / blockSkip)
+    cover.fill(null)
 
     const dictionary: Segment[] = []
 
     while (queue.length) {
         let maxbucket: Bucket = null
         queue = queue.filter(b => {
-            if (b.maxgain <= 0) return false
-            if (!maxbucket || b.maxgain > maxbucket.maxgain)
+            if (!b.max) return false
+            if (!maxbucket || b.max.score > maxbucket.max.score)
                 maxbucket = b
             return true
         })
         if (!maxbucket) break
-        let maxseg: Segment = null
-        for (const s of maxbucket.segments) {
-            if (!maxseg || maxseg.gain < s.gain)
-                maxseg = s
-        }
-
+        let maxseg = maxbucket.max
+        maxseg.idx = dictionary.length
         dictionary.push(maxseg)
-        for (const start of maxseg.allstarts || [maxseg.start]) {
-            for (const s0 of segmentsOverlapping(start, start + maxseg.length)) {
-                // this includes us
-                const overlapSize = Math.min(maxseg.length, s0.length)
-                const s = findSegment(s0)
-                if (s) {
-                    s.gain -= overlapSize
-                    findBucket(s)._maxgain = null
+
+        for (const start of maxseg.allstarts) {
+            if (start < 0) continue
+            if (!sameMem(inputfile, maxseg.start, start, maxseg.length))
+                throw "segment mem different"
+            for (let i = start; i < start + maxseg.length; i += blockSkip) {
+                if (cover[i / blockSkip])
+                    throw ("already covered: " + i)
+                cover[i / blockSkip] = maxseg
+            }
+            for (const other of segsAt[start >> blockSizeBits]) {
+                let numcl = 0
+                for (let i = 0; i < other.allstarts.length; ++i) {
+                    const otherStart = other.allstarts[i]
+                    if (otherStart < 0) continue
+                    if (overlaps(start, maxseg.length, otherStart, other.length)) {
+                        other.allstarts[i] = -1
+                        other.gain -= other.length - refantigain
+                        if (other.gain < -other.length + startgain) {
+                            console.log(other)
+                            throw "mingain"
+                        }
+                        numcl++
+                    }
                 }
+                if (numcl)
+                    findBucket(other).clearCache()
             }
         }
     }
 
     const t2 = Date.now()
-    console.log(`${dictionary.length} entires in dictionary; ${t2 - t1}ms`)
+    console.log(`${dictionary.length} entires in dictionary; ${cover.filter(c => c == null).length * 2} bytes unitary; ${t2 - t1}ms`)
 
-    for (const b of buckets) {
-        b.segments = []
+    const outbytes: number[] = []
+
+    function addbyte(n: number) {
+        if (0 <= n && n <= 0xff) {
+            outbytes.push(n)
+        } else {
+            throw "addbyte:" + n
+        }
     }
+
+    function addshort(n: number) {
+        addbyte(n & 0xff)
+        addbyte(n >> 8)
+    }
+
+    function addlong(n: number) {
+        addbyte(n & 0xff)
+        addbyte((n >> 8) & 0xff)
+        addbyte((n >> 16) & 0xff)
+        addbyte((n >> 24) & 0xff)
+    }
+
+    const posdiv = minsegsize
+    addlong(0x706aef29)
+    const szpos: any = {}
+    for (const sz of segmentSizes) {
+        addshort(sz)
+        szpos[sz] = outbytes.length
+        addshort(0) // patch later        
+    }
+    while (outbytes.length & (posdiv - 1))
+        addbyte(0)
     for (const s of dictionary) {
-        findBucket(s).segments.push(s)
+        for (let i = 0; i < s.length; ++i)
+            addbyte(inputfile[s.start + i])
+        const p = outbytes.length / posdiv
+        if ((p | 0) != p) throw "non-aligned: " + p
+        s.idx = p
+        if (p >= 0x8000) throw "out of range"
+        outbytes[+szpos[s.length]] = p & 0xff
+        outbytes[+szpos[s.length] + 1] = p >> 8
     }
 
+    const numblocks: any = {}
+    const blsize: any = {}
 
+    function addhist(n: string, v: number) {
+        if (!numblocks[n]) {
+            numblocks[n] = 0
+        }
+        numblocks[n]++
+        blsize[n] = v
+    }
+
+    for (let i = 0; i < cover.length;) {
+        const s = cover[i]
+        if (s) {
+            i += s.length >> 1
+            outbytes.push(s.idx & 0xff)
+            outbytes.push(s.idx >> 8)
+            addhist("seg:" + s.length, s.length)
+        } else {
+            const startbyte = i * 2
+            let endbyte = Math.min(startbyte + 0x7f * 2, (startbyte & ~(blockSize - 1)) + blockSize)
+            for (let pos = startbyte; pos < endbyte; pos += 2)
+                if (cover[pos >> 1]) {
+                    endbyte = pos
+                    break
+                }
+            const halfs = (endbyte - startbyte) >> 1
+            if (halfs > 0x7f)
+                throw "unitary:" + halfs
+            addbyte(halfs | 0x80)
+            for (let pos = startbyte; pos < endbyte; pos++)
+                addbyte(inputfile[pos])
+            i += halfs
+            addhist("bytes:" + (halfs * 2), halfs * 2)
+        }
+    }
+
+    function szCover(key: string) {
+        return numblocks[key] * blsize[key]
+    }
+    function szStore(key: string) {
+        if (key[0] == "b")
+            return (blsize[key] + 1) * numblocks[key]
+        else
+            return blsize[key] + 2 * numblocks[key]
+    }
+
+    const keys = Object.keys(numblocks)
+    keys.sort((a, b) => szStore(a) - szStore(b))
+    for (const key of keys) {
+        console.log((szStore(key) * 100 / szCover(key)) | 0, szStore(key), szCover(key), numblocks[key], key)
+    }
+
+    console.log("TOTAL", outbytes.length)
+
+    return new Uint8Array(outbytes)
 }
 
 function concat(inputs: Uint8Array[]) {
@@ -243,11 +376,13 @@ function concat(inputs: Uint8Array[]) {
         total.set(inp, size)
         size += (inp.length + blockSize - 1) & ~(blockSize - 1)
     }
-    compress(total)
+    return compress(total)
 }
 
 declare var Buffer: any
 declare var require: any
 declare var process: any
 const fs = require("fs")
-concat(process.argv.slice(2).map((fn: string) => fs.readFileSync(fn)))
+const arr = concat(process.argv.slice(2).map((fn: string) => fs.readFileSync(fn)))
+fs.writeFileSync("out.seg", Buffer.from(arr))
+
