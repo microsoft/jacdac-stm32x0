807cc77fcc00bc58eae2e2307c166263ed098933
Michal Moskal <michal@moskal.me>
2020-04-20 18:58:50 -0700

    Add CTRL registers for device class, temperature and light level

--- src/ctrl.c
+++ src/ctrl.c
@@ -23,28 +23,60 @@ void ctrl_process(srv_t *_state) {
     identify();
 }
 
+static void send_value(jd_packet_t *pkt, uint32_t v) {
+    txq_push(JD_SERVICE_NUMBER_CTRL, pkt->service_command, &v, sizeof(v));
+}
+
+static uint32_t device_class(void) {
+    uint32_t v = jd_hash_fnv1a(app_dev_class_name, strlen(app_dev_class_name));
+    return (v << 4 >> 4) | (0x3 << 28);
+}
+
 void ctrl_handle_packet(srv_t *_state, jd_packet_t *pkt) {
+    uint32_t v;
+
     switch (pkt->service_command) {
     case JD_CMD_ADVERTISEMENT_DATA:
         app_queue_annouce();
         break;
+
     case JD_CMD_CTRL_IDENTIFY:
         id_counter = 7;
         nextblink = now;
         identify();
         break;
+
     case JD_CMD_CTRL_RESET:
         target_reset();
         break;
+
     case (JD_CMD_GET_REG | JD_REG_CTRL_DEVICE_DESCRIPTION):
         txq_push(JD_SERVICE_NUMBER_CTRL, pkt->service_command, app_dev_class_name,
                  strlen(app_dev_class_name));
         break;
-    case (JD_CMD_GET_REG | JD_REG_CTRL_DEVICE_CLASS): {
-        uint32_t v = jd_hash_fnv1a(app_dev_class_name, strlen(app_dev_class_name));
-        v = (v << 4 >> 4) | (0x3 << 28);
-        txq_push(JD_SERVICE_NUMBER_CTRL, pkt->service_command, &v, sizeof(v));
-    } break;
+
+    case (JD_CMD_GET_REG | JD_REG_CTRL_DEVICE_CLASS):
+        send_value(pkt, device_class());
+        break;
+
+    case (JD_CMD_GET_REG | JD_REG_CTRL_TEMPERATURE):
+        send_value(pkt, adc_read_temp());
+        break;
+
+    // doesn't work on JD-V0 and JDM until V5 - need to swap LED/R positions
+    case (JD_CMD_GET_REG | JD_REG_CTRL_LIGHT_LEVEL):
+        if (PIN_LED_GND != -1) {
+            pin_set(PIN_LED_GND, 1);
+            pin_set(PIN_LED, 0);
+            target_wait_us(5);
+            pin_setup_analog_input(PIN_LED_GND);
+            target_wait_us(500);
+            v = adc_read_pin(PIN_LED_GND);
+            pin_setup_output(PIN_LED_GND);
+            pin_set(PIN_LED_GND, 0);
+            send_value(pkt, v);
+        }
+        break;
     }
 }
 
