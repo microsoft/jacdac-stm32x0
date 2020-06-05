a19349f2d1d9d606a7a1a28d5d385cfb80f43bd8
Michal Moskal <michal@moskal.me>
2020-06-05 07:53:25 -0700

    Add proto board

---
+++ targets/jm-v2.0p/board.h
@@ -0,0 +1,28 @@
+#define PIN_LED PB_1
+#define PIN_LED_GND -1
+#define PIN_LED2 -1
+#define PIN_LOG0 -1 // sig_write
+#define PIN_LOG1 -1
+#define PIN_LOG2 -1 // sig error
+#define PIN_LOG3 -1
+
+#define PIN_PWR -1
+#define PIN_P0 PF_0
+#define PIN_P1 PF_1
+
+#define PIN_SDA PA_10
+#define PIN_SCL PA_9
+#define I2C_AF LL_GPIO_AF_4
+
+#define PIN_ACC_INT PA_7
+
+#define UART_PIN PA_2
+#define UART_PIN_AF LL_GPIO_AF_1
+#define USART_IDX 1
+
+#define PWR_PIN_PULLUP 1
+
+#define OUTPUT_PINS                                                                                \
+    PIN_LOG0, PIN_LOG1, PIN_LOG2, PIN_LOG3, PIN_LED, PIN_LED2, PIN_P0, PIN_P1, PIN_LED_GND
+
+#define ACC_KXTJ3
---
+++ targets/jm-v2.0p/profile/proto.c
@@ -0,0 +1,5 @@
+#include "jdprofile.h"
+
+DEVICE_CLASS(0x3eb65a14, "JM Proto v2.0");
+
+void init_services() {}
