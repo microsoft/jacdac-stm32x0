1e4ed817238eb868ee3b4d060055145946bab8e8
Michal Moskal <michal@moskal.me>
2020-07-09 11:27:10 -0700

    Disable power in PWM+PWR

--- targets/jm-v2.1/profile/npx.c
+++ targets/jm-v2.1/profile/npx.c
@@ -3,8 +3,10 @@
 DEVICE_CLASS(0x3c6bebe4, "JM PWM+PWR (npx) v2.1");
 
 void app_init_services() {
+#if 0
     pin_setup_output(PF_1);
     pin_set(PF_1, 0); // enable JD power
+#endif
     // The sense lines are not connected on this hw revision, so we just skip the actual power service.
     light_init();
 }
