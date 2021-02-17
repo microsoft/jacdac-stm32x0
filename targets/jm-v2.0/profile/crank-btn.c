8dbccc6827b313e75846d2033c0aac317181468d
Tom Ball <tballmsft@users.noreply.github.com>
2021-02-17 00:41:42 -0800

    add clicks_per_turn for rotary encoder (#12)
    * add clicks_per_turn
    
    * update github repo
    
    * redo subrepo
    
    * remove the old submodule

--- targets/jm-v2.0/profile/crank-btn.c
+++ targets/jm-v2.0/profile/crank-btn.c
@@ -3,6 +3,6 @@
 FIRMWARE_IDENTIFIER(0x30838b8e, "JM Crank+Btn v2.0");
 
 void app_init_services() {
-    rotary_init(PA_6, PA_5);
+    rotary_init(PA_6, PA_5, 24);
     btn_init(PA_4, 0, -1);
 }
--- targets/jm-v2.0/profile/crank.c
+++ targets/jm-v2.0/profile/crank.c
@@ -3,5 +3,5 @@
 FIRMWARE_IDENTIFIER(0x33a8780b, "JM Crank v2.0");
 
 void app_init_services() {
-    rotary_init(PA_6, PA_5);
+    rotary_init(PA_6, PA_5, 24);
 }
