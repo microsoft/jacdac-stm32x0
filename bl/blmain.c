5c1abe2e5083e852e772b86485b0a74e314b3013
James Devine <t-jamdev@microsoft.com>
2021-02-25 19:45:45 +0000

    add ifdef guard around enabling usart2 clock

--- bl/blmain.c
+++ bl/blmain.c
@@ -46,7 +46,9 @@ uint32_t bl_adc_random_seed(void);
 int main(void) {
     __disable_irq();
     clk_setup_pll();
+#if USART_IDX==2
     LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART2);
+#endif
     LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_TIM17 | LL_APB1_GRP2_PERIPH_USART1 |
                              LL_APB1_GRP2_PERIPH_ADC1);
 
