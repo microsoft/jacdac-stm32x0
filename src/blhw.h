5a5ab67e1bb4c88c8d2075fb69fb7fe4149f757d
Michal Moskal <michal@moskal.me>
2021-03-05 17:39:44 -0800

    Rename FLASH_SIZE to JD_FLASH_SIZE (conflict with HAL)

--- src/blhw.h
+++ src/blhw.h
@@ -74,5 +74,5 @@ struct app_top_handlers {
 #define app_dev_info app_handlers->devinfo
 
 #ifndef BL
-#define bl_info (*((struct bl_info_block *)(0x8000000 + FLASH_SIZE - BL_SIZE)))
+#define bl_info (*((struct bl_info_block *)(0x8000000 + JD_FLASH_SIZE - BL_SIZE)))
 #endif
