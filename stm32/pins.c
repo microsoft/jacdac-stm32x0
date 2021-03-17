0db5cbb5ea576c9693cc1f40dcc63559445c172d
Michal Moskal <michal@moskal.me>
2021-03-17 11:39:37 -0700

    Don't use LL_GPIO_Init()

--- stm32/pins.c
+++ stm32/pins.c
@@ -35,33 +35,40 @@ void pin_setup_input(int pin, int pull) {
     uint32_t currentpin = PIN_MASK(pin);
     LL_GPIO_SetPinMode(GPIOx, currentpin, LL_GPIO_MODE_INPUT);
     LL_GPIO_SetPinPull(GPIOx, currentpin,
-                       pull == -1 ? LL_GPIO_PULL_DOWN
-                                  : pull == 1 ? LL_GPIO_PULL_UP
-                                              : pull == 0 ? LL_GPIO_PULL_NO : (jd_panic(), 0));
+                       pull == -1  ? LL_GPIO_PULL_DOWN
+                       : pull == 1 ? LL_GPIO_PULL_UP
+                       : pull == 0 ? LL_GPIO_PULL_NO
+                                   : (jd_panic(), 0));
 }
 
 void pin_setup_analog_input(int pin) {
     if ((uint8_t)pin == 0xff)
         return;
-    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Pin = PIN_MASK(pin);
-    LL_GPIO_Init(PIN_PORT(pin), &GPIO_InitStruct);
+
+    GPIO_TypeDef *GPIOx = PIN_PORT(pin);
+    uint32_t currentpin = PIN_MASK(pin);
+    LL_GPIO_SetPinPull(GPIOx, currentpin, LL_GPIO_PULL_NO);
+    LL_GPIO_SetPinMode(GPIOx, currentpin, LL_GPIO_MODE_ANALOG);
 }
 
 // TODO use this everywhere
 void pin_setup_output_af(int pin, int af) {
     if ((uint8_t)pin == 0xff)
         return;
-    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = PIN_MASK(pin);
-    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-    GPIO_InitStruct.Pull = LL_GPIO_PULL_DOWN;
-    GPIO_InitStruct.Alternate = af;
-    LL_GPIO_Init(PIN_PORT(pin), &GPIO_InitStruct);
+
+    GPIO_TypeDef *GPIOx = PIN_PORT(pin);
+    uint32_t currentpin = PIN_MASK(pin);
+
+    LL_GPIO_SetPinSpeed(GPIOx, currentpin, LL_GPIO_SPEED_FREQ_HIGH);
+    LL_GPIO_SetPinOutputType(GPIOx, currentpin, LL_GPIO_OUTPUT_PUSHPULL);
+    LL_GPIO_SetPinPull(GPIOx, currentpin, LL_GPIO_PULL_DOWN);
+
+    if (currentpin < LL_GPIO_PIN_8)
+        LL_GPIO_SetAFPin_0_7(GPIOx, currentpin, af);
+    else
+        LL_GPIO_SetAFPin_8_15(GPIOx, currentpin, af);
+
+    LL_GPIO_SetPinMode(GPIOx, currentpin, LL_GPIO_MODE_ALTERNATE);
 }
 
 void pin_pulse(int pin, int times) {
