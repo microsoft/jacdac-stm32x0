5a86aa12fd2046d95c8deb9fef9bae3594695c0a
Michal Moskal <michal@moskal.me>
2022-02-16 18:00:19 -0800

    de-HALify

--- stm32/lora-e5/subghz.c
+++ stm32/lora-e5/subghz.c
@@ -17,37 +17,27 @@
  ******************************************************************************
  */
 
-/* Includes ------------------------------------------------------------------*/
+#include "jdstm.h"
 #include "subghz.h"
 
 SUBGHZ_HandleTypeDef hsubghz;
 
-/* SUBGHZ init function */
 void MX_SUBGHZ_Init(void) {
-
     hsubghz.Init.BaudratePrescaler = SUBGHZSPI_BAUDRATEPRESCALER_4;
     if (HAL_SUBGHZ_Init(&hsubghz) != HAL_OK) {
-        Error_Handler();
+        jd_panic();
     }
 }
 
 void HAL_SUBGHZ_MspInit(SUBGHZ_HandleTypeDef *subghzHandle) {
-
-    /* SUBGHZ clock enable */
     __HAL_RCC_SUBGHZSPI_CLK_ENABLE();
-
-    /* SUBGHZ interrupt Init */
-    HAL_NVIC_SetPriority(SUBGHZ_Radio_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(SUBGHZ_Radio_IRQn);
+    NVIC_SetPriority(SUBGHZ_Radio_IRQn, 3);
+    NVIC_EnableIRQ(SUBGHZ_Radio_IRQn);
 }
 
 void HAL_SUBGHZ_MspDeInit(SUBGHZ_HandleTypeDef *subghzHandle) {
-
-    /* Peripheral clock disable */
     __HAL_RCC_SUBGHZSPI_CLK_DISABLE();
-
-    /* SUBGHZ interrupt Deinit */
-    HAL_NVIC_DisableIRQ(SUBGHZ_Radio_IRQn);
+    NVIC_DisableIRQ(SUBGHZ_Radio_IRQn);
 }
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
