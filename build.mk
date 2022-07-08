1ed84560eac9d8bfb9626451a3993c682734eddd
Michal Moskal <michal@moskal.me>
2022-07-08 10:44:51 -0700

    try to fix windows build

--- build.mk
+++ build.mk
@@ -317,8 +317,8 @@ endif
 $(BUILT_BIN)/$(PREF)-%.elf: $(PROF_DEP)-%.o $(OBJ) Makefile $(LD_SCRIPT) $(SCRIPTS)/patch-bin.js $(FORCE)
 	@echo LD $@
 ifeq ($(OS), Windows_NT)
-	$(V)echo $(OBJ) $< -lm > $(BUILT_BIN)/$(PREF)-flags.txt
-	$(V)$(CC) $(CFLAGS) $(LDFLAGS) -Wl,-Map=$@.map -o $@ @$(BUILT_BIN)/$(PREF)-flags.txt
+	$(file >$(BUILT_BIN)/$(PREF)-flags.txt,$(OBJ))
+	$(V)$(CC) $(CFLAGS) $(LDFLAGS) -Wl,-Map=$@.map -o $@ @$(BUILT_BIN)/$(PREF)-flags.txt $< -lm
 else
 	$(V)$(CC) $(CFLAGS) $(LDFLAGS) -Wl,-Map=$@.map  -o $@ $(OBJ) $< -lm
 endif
