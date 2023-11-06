78423b0d4c089e3e451f2bf233bbe86d7047fe90
Mark Olsson <mark@markolsson.se>
2023-11-06 18:53:24 +0100

    add BL support for PB_0 LED (#48)

--- bl/blpwm.c
+++ bl/blpwm.c
@@ -50,6 +50,11 @@
 #define TIMx_CLK_ENABLE __HAL_RCC_TIM16_CLK_ENABLE
 #define CHANNEL 1
 #define AF LL_GPIO_AF_2
+#elif PIN_BL_LED == PB_0
+#define TIMx TIM3
+#define TIMx_CLK_ENABLE __HAL_RCC_TIM3_CLK_ENABLE
+#define CHANNEL 3
+#define AF LL_GPIO_AF_1
 #endif
 #else
 #error "unsupported MCU"
